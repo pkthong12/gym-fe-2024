1f7e37fc1a96f67b480056c6359c690e0b42d67e
pkthong12 <129492809+pkthong12@users.noreply.github.com>
2025-04-23 21:36:15 +0700

    update

---
+++ firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDIBxv9f24dECQtqe4RNL08gFzJr_f9mmg",
+  authDomain: "gym-help.firebaseapp.com",
+  projectId: "gym-help",
+  storageBucket: "gym-help.firebasestorage.app",
+  messagingSenderId: "138627924905",
+  appId: "1:138627924905:web:0e6b09789e5c113973bab8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
